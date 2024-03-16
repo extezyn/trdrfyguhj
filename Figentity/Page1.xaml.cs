7a095cfbb75c15163f3fc88d2c62f617f323b7c4
extezyn <144113053+extezyn@users.noreply.github.com>
2024-03-16 13:12:18 +0300

    asd

---
+++ Figentity/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Figentity
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        private FitnessClub1Entities fit = new FitnessClub1Entities();
+        public Page1()
+        {
+            InitializeComponent();
+            getgrid.ItemsSource = fit.Members.ToList();
+        }
+    }
+}
